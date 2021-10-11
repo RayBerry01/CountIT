66b741e1465acd018241caa0de481ca0fb62064f
JARBerry <rayberryrb@gmail.com>
2021-10-11 20:14:15 +0100

    Initial Commit

---
+++ CountIT/CountITApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountITApp.swift
+//  CountIT
+//
+//  Created by Ray Berry on 11/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CountITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
